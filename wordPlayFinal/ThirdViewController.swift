9b8ea55591a4a882d56ff16e738327a6e603c8d0
Julio Rivera <jrivera0325@jh-mba158-16.local>
2018-12-07 11:42:09 -0600

    Started Stretch One

---
+++ wordPlayFinal/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  wordPlayFinal
+//
+//  Created by Julio Rivera on 12/7/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
