f54ad5d61ea82eb9b06254563353e3a6e8d46d46
Julio Rivera <jrivera0325@jh-mba158-16.local>
2018-12-07 11:29:02 -0600

    MVP complete

--- wordPlayFinal/ViewController.swift
+++ wordPlayFinal/ViewController.swift
@@ -6,20 +6,25 @@
 //  Copyright © 2018 John Hersey High School. All rights reserved.
 //
 
-import UIKit
+import UIKit 
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var myLabel: UILabel!
-    @IBOutlet weak var mySentence: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var textField1: UITextField!
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var secondController = segue.destination as! secondViewController
+        secondController.myString = textField1.text ?? " "
+    
         
+        func viewDidLoad() {
+        super.viewDidLoad()
     }
     
-    @IBAction func fullSentence(_ sender: Any) {
-    
     }
-    
+
+
 }
 
--- wordPlayFinal/secondViewController.swift
+++ wordPlayFinal/secondViewController.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class secondViewController: UIViewController {
-
+    @IBOutlet weak var myLabel2: UILabel!
+    
+    var myString = String()
     override func viewDidLoad() {
         super.viewDidLoad()
+        myLabel2.text = "My uncle wants to go to the " + myString 
 
         // Do any additional setup after loading the view.
     }
+
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
